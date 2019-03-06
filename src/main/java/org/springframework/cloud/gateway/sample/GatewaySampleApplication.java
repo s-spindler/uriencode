449bcc4ee52f3ba802af9d5fca5d0255fb0ff922
Stephan Spindler <stephan.spindler@device-insight.com>
2019-03-06 17:07:48 +0100

    Create minimal example for uri encoding issue

---
+++ src/main/java/org/springframework/cloud/gateway/sample/GatewaySampleApplication.java
@@ -0,0 +1,33 @@
+package org.springframework.cloud.gateway.sample;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration
+@EnableAutoConfiguration
+public class GatewaySampleApplication {
+
+	@Value("${test.uri:http://httpbin.org:80}")
+	String uri;
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		//@formatter:off
+		return builder.routes()
+				.route(r -> r.path("/anything/*")
+					.filters(f -> f.prefixPath("/httpbin"))
+					.uri(uri)
+				)
+				.build();
+		//@formatter:on
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewaySampleApplication.class, args);
+	}
+}
---
+++ src/test/java/org/springframework/cloud/gateway/sample/GatewaySampleApplicationTests.java
@@ -0,0 +1,58 @@
+package org.springframework.cloud.gateway.sample;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.SocketUtils;
+
+import java.time.Duration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class GatewaySampleApplicationTests {
+
+	@LocalServerPort
+	protected int port = 0;
+
+	protected static int managementPort;
+
+	protected WebTestClient webClient;
+	protected String baseUri;
+
+	@BeforeClass
+	public static void beforeClass() {
+		managementPort = SocketUtils.findAvailableTcpPort();
+
+		System.setProperty("management.server.port", String.valueOf(managementPort));
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		System.clearProperty("management.server.port");
+	}
+
+	@Before
+	public void setup() {
+		baseUri = "http://localhost:" + port;
+		this.webClient = WebTestClient.bindToServer().responseTimeout(Duration.ofSeconds(10)).baseUrl(baseUri).build();
+	}
+
+	@Test
+	public void testWithoutPercentage() {
+		webClient.get().uri("/anything/100").exchange().expectStatus().isOk();
+	}
+
+	@Test
+	public void testWithPercentage() {
+		webClient.get().uri("/anything/100%").exchange().expectStatus().isOk();
+	}
+
+}
